9d88c604e43d49650c98311605e4be5f78d5d52f
rguha13 <142922477+rguha13@users.noreply.github.com>
2023-10-17 23:59:00 -0500

    Update FINAL_FUNCTIONS_chi2_test.R
    Added to the description and an additional example for setting r

--- R/FINAL_FUNCTIONS_chi2_test.R
+++ R/FINAL_FUNCTIONS_chi2_test.R
@@ -96,8 +96,10 @@ log_G_frac = function(tau, h, k, r)
 
 #' chi2.test.BFF
 #'
-#' Chi^2 test using BFF methods. Setting r > 1 uses the higher order moments. Fractional moments are set with r > 1 and r not an integer.
-#' All results are on the log scale. Plot saved to working directory unless a full path is specified in the 'savename' variable.
+#' Constructing BFFs based on the Chi-squared test. BFFs depend on hyperparameters r and tau^2 which determine the shape and scale of the prior distributions which define the alternative hypotheses. 
+#' By setting r > 1, we use higher-order moments. Fractional moments are set with r > 1 and r not an integer.
+#' All results are on the log scale. 
+#' Plot saved to working directory unless a full path is specified in the 'savename' variable of the function.
 #'
 #' @param chi2_stat chi^2 statistic
 #' @param df degrees of freedom
@@ -125,7 +127,8 @@ log_G_frac = function(tau, h, k, r)
 #' @export
 #'
 #' @examples
-#' chi2.test.BFF(chi2_stat=2.5, n = 50, df = 20, save=FALSE)
+#' chi2.test.BFF(chi2_stat = 2.5, n = 50, df = 20, save = FALSE)
+#' chi2.test.BFF(chi2_stat = 3.5, n = 35, df = 10, r = 2, save = FALSE)
 chi2.test.BFF = function(chi2_stat,
                       n = NULL,
                       df = NULL,
