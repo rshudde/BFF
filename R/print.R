a72c456b8b51384e8e240a59e5ff54726fff206c
rshudde <rachael.shudde@gmail.com>
2024-02-27 17:08:46 -0600

    added cases for f and chi2

--- R/print.R
+++ R/print.R
@@ -21,16 +21,25 @@ summary.BFF <- function(object, ...){
 #' @seealso [z_test_BFF()], [t_test_BFF()], [chi2_test_BFF()], [f_test_BFF()]
 #' @export
 print.BFF = function(x, ...) {
-  cat(paste0("\t\t", .test_type_name(x$test_type, x$one_sample)))
-  cat("\n\n")
-  cat(gettextf("%1$slog Bayes factor = %2$.2f\n", if(!x$omega_set) "maximized " else "", x$log_bf))
-  cat(gettextf("%1$slog tau2 = %2$.2f\n", if(!x$omega_set) "maximized " else "", x$omega))
-  cat(paste0("alternative = ", x$alternative))
+    cat(paste0("\t\t", .test_type_name(x$test_type, x$one_sample)))
+    cat("\n\n")
+    cat(gettextf("%1$slog Bayes factor = %2$.2f\n", if(!x$omega_set) "maximized " else "", x$log_bf))
+    cat(gettextf("%1$slog tau2 = %2$.2f\n", if(!x$omega_set) "maximized " else "", x$omega))
+    if (!is.null(x$one_sample)) cat(paste0("alternative = ", x$alternative))
 }
 
 .test_type_name = function(test_type, one_sample) {
-  starting_strng = gettextf("Bayesian non-local %1$s %2$s",
-                            if(one_sample) "one-sample" else "two-sample",
+  if (is.null(one_sample)) {
+    sided = ''
+  } else {
+    if(one_sample)
+    {
+      sided = "one-sample"
+    } else{
+      sided = 'two-sample'
+    }
+  }
+  starting_strng = gettextf("Bayesian non-local %1$s %2$s", sided,
                             switch(test_type,
                                    "t_test"    = "t test",
                                    "z_test"    = "z test",
